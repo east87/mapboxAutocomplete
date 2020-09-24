8074637412ca301150c37836a2bbda2db6e1552c
east87 <hl.prbadolsa@gmail.com>
2020-09-25 00:34:44 +0700

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,40 @@
+<template>
+  <mapbox-map
+    style="height: 400px"
+    :access-token="accesstoken"
+    map-style="mapbox://styles/mapbox/streets-v11"
+    :center="[0, 0]"
+    :zoom="1">
+    <mapbox-marker :lng-lat="[0, 0]" popup>
+      <template v-slot:popup>
+        <p>Hello world!</p>
+      </template>
+    </mapbox-marker>
+  </mapbox-map>
+</template>
+
+<script>
+//import mapboxgl from 'mapbox-gl';
+import { MapboxMap, MapboxMarker } from '@studiometa/vue-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+export default {
+  name: "geocoder",
+  components: {
+  //  mapboxgl,
+ MapboxMap,
+ MapboxMarker
+  },
+
+  data() {
+    return {
+      accesstoken: "pk.eyJ1IjoiZWFzdDg3IiwiYSI6ImNrZjUzNHFlcjA0d2cyeWp4OWliYTkwdDcifQ.oovHlSDtUv7JTimvnpRl2Q",
+      
+    };
+  },
+  methods: {
+    onComplete() {
+      alert("Submitting Form ! Rock On");
+    },
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <mapbox-geocoder :access-token="accesstoken" @mb-results="onComplete" />
+</template>
+
+<script>
+//import mapboxgl from 'mapbox-gl';
+import { MapboxGeocoder } from '@studiometa/vue-mapbox-gl';
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+export default {
+  name: "geocoder",
+  components: {
+  //  mapboxgl,
+ MapboxGeocoder
+  },
+
+  data() {
+    return {
+      accesstoken: "pk.eyJ1IjoiZWFzdDg3IiwiYSI6ImNrZjUzNHFlcjA0d2cyeWp4OWliYTkwdDcifQ.oovHlSDtUv7JTimvnpRl2Q",
+      
+    };
+  },
+  methods: {
+    onComplete(results) {
+      console.log(results);
+    },
+  },
+};
+</script>
